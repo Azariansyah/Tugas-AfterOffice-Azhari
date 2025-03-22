913d822d04b45aed06af75e13574b137f39aa766
Azhari Iriansyah <azhari.iriansyah@gmail.com>
2025-03-22 19:36:00 +0700

    Config POM.XML

--- src/test/java/runners/TestRunnerTestNG.java
+++ src/test/java/runners/TestRunnerTestNG.java
@@ -1,12 +1,10 @@
 package runners;
 
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
-import org.junit.runner.RunWith;
+import io.cucumber.testng.CucumberOptions;
 
-@RunWith(Cucumber.class)
+
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"stepdefinitions", "hook"},
